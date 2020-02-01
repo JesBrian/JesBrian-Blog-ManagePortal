fa1530a92c644deefd7758b6ee417f1b074a3d4e
JesBrian <jesbrian666@gmail.com>
2020-02-01 14:10:06 +0800

    project - [ 引入 vuex 和 vue-router ]

--- index.view.js
+++ index.view.js
@@ -1,7 +1,21 @@
+
+// 引入 Vue
 import Vue from 'vue';
+
+// 引入 VueRouter
+import router from './src/router';
+
+// 引入 Vuex
+import store from './src/store';
+
+// 引入 App 根组件
 import App from './App.vue';
 
-
 new Vue({
-  render: h => h(App)
+  router,
+  store,
+  data: {
+    eventHub: new Vue()
+  },
+  render: h => h(App),
 }).$mount('#app');
