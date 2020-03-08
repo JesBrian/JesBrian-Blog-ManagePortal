ae695aab001ad6b2d99beaddebb46a5923e75233
JesBrian <jesbrian666@gmail.com>
2020-03-08 17:18:36 +0800

    request - [ import axios ]

---
+++ src/views/Pages/WebPage/WebPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="WebPage">
+    <pre v-text="$attrs"/>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {},
+  };
+</script>
+
+<style lang="less" scoped >
+  @import "WebPage.less";
+</style>
